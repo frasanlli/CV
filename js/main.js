2ad3efff03f4446f9dd6a9a0c28c845bf6eaf723
frasanlli <146319006+frasanlli@users.noreply.github.com>
2026-06-05 22:28:31 +0200

    1.6.1 added partial support for keyboard

--- js/main.js
+++ js/main.js
@@ -11,35 +11,53 @@ function prepararMail() {
   window.location.href = mailto;
 }
 
-function changeSlide(step) {
-  const slides = document.querySelectorAll(".promo-card");
+function getActiveIndex(querySelectorAll, activeClass, step){
+  const searchedElements = document.querySelectorAll(querySelectorAll);
   let currentIndex = 0;
-
-  for (let i = 0; i < slides.length; i++) {
-    if (slides[i].classList.contains("active")) {
+  for (let i = 0; i < searchedElements.length; i++) {
+    if (searchedElements[i].classList.contains(activeClass)) {
       currentIndex = i + step;
       break;
     }
   }
-
-  if (currentIndex >= slides.length) {
+  if (currentIndex >= searchedElements.length) {
     currentIndex = 0;
   } else if (currentIndex < 0) {
-    currentIndex = slides.length - 1;
+    currentIndex = searchedElements.length - 1;
   }
-
-  slides.forEach(slide => slide.classList.remove("active"));
-  slides[currentIndex].classList.add("active");
+  return currentIndex;
 }
 
-async function cargarPagina(btn) {
+function changeSlide(step) {
+  const querySlides = ".promo-card"
+  const activeClass = "active"
+  const searchedElements = document.querySelectorAll(querySlides);
+  const currentIndex = getActiveIndex(querySlides, activeClass, step);
+  searchedElements.forEach(searchedElement => searchedElement.classList.remove(activeClass));
+  searchedElements[currentIndex].classList.add(activeClass);
+  void searchedElements[currentIndex].offsetWidth;
+}
+
+function changeTab(){
+  const querySlides = ".tab"
+  const activeSlide = "activo"
+  const searchedElements = document.querySelectorAll(querySlides);
+  const currentIndex = getActiveIndex(querySlides, activeSlide, 1);
+  console.log(searchedElements[currentIndex].tagName)
+  if (searchedElements[currentIndex].tagName != "BUTTON"){
+    loadPage(searchedElements[0])
+  }else{
+    loadPage(searchedElements[currentIndex])
+  }
+}
+
+async function loadPage(btn) {
   const allTabs = document.querySelectorAll(".tab");
   allTabs.forEach(tab => tab.classList.remove("activo"));
   btn.classList.add("activo");
 
   const nombre = btn.dataset.page;
   const respuesta = await fetch(`pages/${nombre}.html`);
-
   const html = await respuesta.text();
 
   app.innerHTML = html;
@@ -48,11 +66,11 @@ async function cargarPagina(btn) {
 }
 
 const app = document.querySelector("main.panel");
-  const btn_proyectos = document.querySelector("button[data-page='proyectos']");
+const btn_proyectos = document.querySelector("button[data-page='proyectos']");
 
 document.addEventListener("DOMContentLoaded", async () => {
   await i18n.init();
-  cargarPagina(btn_proyectos);
+  loadPage(btn_proyectos);
 });
 
 document.getElementById("langSelect").addEventListener("change", async (e) => {
@@ -61,14 +79,29 @@ document.getElementById("langSelect").addEventListener("change", async (e) => {
 });
 
 document.querySelectorAll("button").forEach(btn => {
-
-  btn.addEventListener("click", () => {
-    if (btn.dataset.page){
-      cargarPagina(btn);
-    }
-  });
-
+  if (btn.dataset.page){
+    btn.addEventListener("click", () => {
+      if (!btn.classList.contains("activo")){
+        loadPage(btn);
+      }
+    });
+  }
 });
 
 
+document.addEventListener("keydown", function(event) {
+  //console.log(event.key.toLowerCase())
+  switch (event.key.toLowerCase()) {
+    case "arrowright":
+      document.getElementById("next").click();
+      break;
 
+    case "arrowleft":
+      document.getElementById("prev").click();
+      break;
+
+    case "tab":
+      changeTab()
+      break;
+  }
+});
